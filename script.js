9cd92258d1e10cd3717a1d9f1562ee7116cc1ab6
agnews <64598944+agnews@users.noreply.github.com>
2020-09-21 22:41:51 -0400

    idk

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function displayNextImage2() {
 
 function startTimer() {
     setInterval(displayNextImage, 3000);
-    setInterval(displayNextImage2, 5000);
+    setInterval(displayNextImage2, 7000);
 }
 
 var images = [], x = -1;
@@ -19,7 +19,7 @@ images[1] = "greyhound 1.jpg";
 images[2] = "bee.jpg";
 images[3] = "vertiblock1.jpg";
 images[4] = "pollock.jpg";
-images[5] = "neo-sheep.jpg";
+images[5] = "neosheep.jpg";
 images[6] = "lettera.jpg";
 images[7] = "hummingbird.jpg";
 images[8] = "honeycomb.jpg";
@@ -34,3 +34,4 @@ images2[5] = "./textexcerpts/e6.png";
 images2[6] = "./textexcerpts/e7.png";
 images2[7] = "./textexcerpts/e8.png";
 images2[8] = "./textexcerpts/e9.png";
+images2[9] = "./textexcerpts/e10.png";
